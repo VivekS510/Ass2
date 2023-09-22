89c3900c984037bc11e88b9e48bda14317e138d8
Vivek <svivek1507@gmail.com>
2023-09-21 23:22:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 30;
+            Console.WriteLine("Enter first Value: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Value: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            if (first == c || second == c || first + second == 30)
+            {
+                Console.WriteLine("True");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("False");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
